cc623a845466fa998dcb94dd8f67dfcfa43b0fd8
huangyuehao <huangyuehao@xhqb.com>
2022-10-25 11:43:29 +0800

    update redis pom

---
+++ redis-module/src/main/java/com/hoshino/study/redis/RedisApplication.java
@@ -0,0 +1,15 @@
+package com.hoshino.study.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangyuehao
+ * @date 2022-10-25
+ */
+@SpringBootApplication
+public class RedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class, args);
+    }
+}
