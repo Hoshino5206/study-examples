fd3f36e11f1d19fb2a35740b38832cf0c71b5784
huangyuehao <huangyuehao@xhqb.com>
2022-11-08 15:59:28 +0800

    init sso

---
+++ example-sso/src/main/java/com/hoshino/example/sso/Application.java
@@ -0,0 +1,17 @@
+package com.hoshino.example.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangyuehao
+ * @date 2022-11-07
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
