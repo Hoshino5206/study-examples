3498c207b90e4adb8bd12511fd4826ddc40e3980
huangyuehao <huangyuehao@xhqb.com>
2022-12-06 18:33:39 +0800

    update spring security

---
+++ example-spring-security/src/main/java/com/hoshino/example/security/Application.java
@@ -0,0 +1,15 @@
+package com.hoshino.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangyuehao
+ * @date 2022-11-30
+ */
+@SpringBootApplication
+public class Application {
+    public static void main (String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
