bdbdc0a30ca65d7b7e9f231bd3da8960b0e7367d
huangyuehao <huangyuehao@xhqb.com>
2022-12-12 19:50:58 +0800

    add springboot run

--- example-springboot-starter-logback/src/main/java/com/hoshino/example/springboot/starter/logback/Application.java
+++ example-springboot-starter-logback/src/main/java/com/hoshino/example/springboot/starter/logback/Application.java
@@ -1,5 +1,6 @@
 package com.hoshino.example.springboot.starter.logback;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2022-12-12
  */
 @SpringBootApplication
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("password:{}", "dqui&2bnijS%89BS61.sq8&s");
     }
 }
