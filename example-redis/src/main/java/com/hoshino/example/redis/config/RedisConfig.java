4b0226eb7479a9998bf2deedc295f10d2b0fe8ec
huangyuehao <huangyuehao@xhqb.com>
2022-12-16 17:05:19 +0800

    udpate redis config and pom

--- example-redis/src/main/java/com/hoshino/example/redis/config/RedisConfig.java
+++ example-redis/src/main/java/com/hoshino/example/redis/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.hoshino.example.redis.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,6 +16,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Configuration
 public class RedisConfig {
+
     /**
      * redisTemplate 序列化使用的jdkSerializeable, 存储二进制字节码, 所以自定义序列化类
      * @param redisConnectionFactory redis连接工厂类
@@ -33,9 +37,17 @@ public class RedisConfig {
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
 
         // 设置value的序列化规则和 key的序列化规则
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
         redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
+
 }
---
+++ example-redis/src/main/java/com/hoshino/example/redis/service/UserService.java
@@ -0,0 +1,34 @@
+package com.hoshino.example.redis.service;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author huangyuehao
+ * @date 2022-12-16
+ */
+@Service
+public class UserService {
+
+    private RedisTemplate<String, String> redisTemplate;
+
+    public void add() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void get() {
+
+    }
+
+    public void set() {
+
+    }
+}
