132f5c90aa17dd25beceb1ad88eed261bdc06b34
Hoshino5206 <1077803190@qq.com>
2022-12-20 14:03:41 +0800

    update redis client and pom

--- example-redis/src/main/java/com/hoshino/example/redis/config/RedisConfig.java
+++ example-redis/src/main/java/com/hoshino/example/redis/config/RedisConfig.java
@@ -6,19 +6,12 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
 
 /**
  * @author huangyuehao
@@ -43,36 +36,19 @@ public class RedisConfig {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
 //        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance ,
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
                 ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-
         jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-//        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-//        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
 
         // 使用StringRedisSerializer来序列化和反序列化redis的key值
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
 
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
-    }
-
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        // 生成一个默认配置，通过config对象即可对缓存进行自定义配置
-        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-        // 使用Jackson2JsnRedisSerializer来序列化和反序列化redis的value值
-        Jackson2JsonRedisSerializer<JSON> serializer = new Jackson2JsonRedisSerializer<>(JSON.class);
-        // 配置序列化
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
-        config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer));
-        config.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
-        // 设置缓存的默认过期时间
-        config.entryTtl(Duration.ofSeconds(60));
-        // 不缓存空值
-        config.disableCachingNullValues();
-        return  RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(config).build();
     }
 
 }
