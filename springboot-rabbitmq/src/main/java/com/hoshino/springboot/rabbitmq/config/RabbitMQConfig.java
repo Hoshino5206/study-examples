e628131c4b7db5ccb918ebbeab58a2b7ede6e34e
Hoshino5206 <1077803190@qq.com>
2023-01-05 15:18:23 +0800

    RabbitMQ可靠性投递

--- springboot-rabbitmq/src/main/java/com/hoshino/springboot/rabbitmq/config/RabbitMQConfig.java
+++ springboot-rabbitmq/src/main/java/com/hoshino/springboot/rabbitmq/config/RabbitMQConfig.java
@@ -98,7 +98,7 @@ public class RabbitMQConfig {
      */
     @Bean
     public TopicExchange topicExchange() {
-        return new TopicExchange(TOPIC_EXCHANGE);
+        return new TopicExchange(TOPIC_EXCHANGE, true, false, null);
     }
 
 
@@ -137,9 +137,6 @@ public class RabbitMQConfig {
      */
     @Bean
     public Queue fanoutQueueA() {
-        // durable:是否持久化,默认是false,持久化队列：会被存储在磁盘上，当消息代理重启时仍然存在，暂存队列：当前连接有效
-        // exclusive:默认也是false，只能被当前创建的连接使用，而且当连接关闭后队列即被删除。此参考优先级高于durable
-        // autoDelete:是否自动删除，当没有生产者或者消费者使用此队列，该队列会自动删除。
         return new Queue(FANOUT_QUEUE_A, true, false, false, null);
     }
 
---
+++ springboot-rabbitmq/src/main/java/com/hoshino/springboot/rabbitmq/provider/ProviderMessageAckController.java
@@ -0,0 +1,82 @@
+package com.hoshino.springboot.rabbitmq.provider;
+
+import com.hoshino.springboot.rabbitmq.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * RabbitMQ在保证消息可靠投递.
+ * 从总体的情况分析，生产者推送消息的消息确认，推送消息存在四种情况：
+ * ①消息推送到server，但是在server里找不到交换机
+ * ②消息推送到server，找到交换机了，但是没找到队列
+ * ③消息推送到sever，交换机和队列都没找到
+ * ④消息推送成功
+ * @author huangyuehao
+ * @date 2023-01-05
+ */
+@RestController
+public class ProviderMessageAckController {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    /**
+     * ①消息推送到server，但是在server里找不到交换机.
+     * 这种情况触发的是 ConfirmCallback 回调函数.
+     */
+    @GetMapping("/sendDirectMessageAck1")
+    public String sendDirectMessageAck1() {
+        rabbitTemplate.convertAndSend("non-existent-exchange", RabbitMQConfig.DIRECT_ROUTING_KEY_A, getMessage("non-existent-exchange"));
+        return "ok";
+    }
+
+    /**
+     * ②消息推送到server，找到交换机了，但是没找到队列.
+     * 这种情况触发的是 ConfirmCallback 和 ReturnCallback 两个回调函数.
+     */
+    @GetMapping("/sendDirectMessageAck2")
+    public String sendDirectMessageAck2() {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.DIRECT_EXCHANGE, "non-existent-routingKey", getMessage("non-existent-queue"));
+        return "ok";
+    }
+
+    /**
+     * ③消息推送到sever，交换机和队列都没找到.
+     * 这种情况触发的是 ConfirmCallback 回调函数.
+     */
+    @GetMapping("/sendDirectMessageAck3")
+    public String sendDirectMessageAck3() {
+        rabbitTemplate.convertAndSend("non-existent-exchange", "non-existent-routingKey", getMessage("non-existent-exchange and queue"));
+        return "ok";
+    }
+
+    /**
+     * ④消息推送成功
+     * 这种情况触发的是 ConfirmCallback 回调函数.
+     */
+    @GetMapping("/sendDirectMessageAck4")
+    public String sendDirectMessageAck4() {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.DIRECT_EXCHANGE, RabbitMQConfig.DIRECT_ROUTING_KEY_A, getMessage("direct queue A"));
+        return "ok";
+    }
+
+    public Map<String, String> getMessage(String message) {
+        String messageId = String.valueOf(UUID.randomUUID());
+        String messageData = message + " !";
+        String createTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        Map<String, String> map = new HashMap<String, String>(8);
+        map.put("messageId", messageId);
+        map.put("messageData", messageData);
+        map.put("createTime", createTime);
+        return map;
+    }
+
+}
