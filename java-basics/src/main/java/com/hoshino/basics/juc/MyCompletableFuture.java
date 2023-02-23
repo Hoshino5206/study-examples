03b2e22cb4dfebd4281f5ff14455d3ab3b2368da
huangyuehao <huangyuehao@xhqb.com>
2023-02-23 17:46:32 +0800

    (java-basics): update CompletableFuture

--- java-basics/src/main/java/com/hoshino/basics/juc/MyCompletableFuture.java
+++ java-basics/src/main/java/com/hoshino/basics/juc/MyCompletableFuture.java
@@ -27,6 +27,7 @@ public class MyCompletableFuture {
                 CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.MINUTES,
                 ARRAY_BLOCKING_QUEUE, THREAD_FACTORY, ABORT_POLICY);
 
+        System.out.println("=================================================================");
         // 创建无返回值的异步任务，runAsync()
         for (int i = 0; i < 6; i++) {
             try {
@@ -39,7 +40,6 @@ public class MyCompletableFuture {
         }
 
         System.out.println("=================================================================");
-
         // 创建有返回值的异步任务，supplyAsync()
         for (int i = 0; i < 6; i++) {
             try {
@@ -47,11 +47,76 @@ public class MyCompletableFuture {
                     System.out.println(Thread.currentThread().getName() + " 正在运行 CompletableFuture.supplyAsync方法");
                     return Thread.currentThread().getName() + " 已执行 CompletableFuture.supplyAsync方法";
                 }, threadPoolExecutor);
-                // 可以通过get或者join方法，阻塞等待并从CompletableFuture中获取对应的值,join()不抛异常的阻塞时获取执行结果
+                // 可以通过get或者join方法，阻塞等待并从CompletableFuture中获取对应的值，join()不抛异常的阻塞时获取执行结果
                 System.out.println(rFuture.get());
             } catch (ExecutionException | InterruptedException e){
                 e.printStackTrace();
             }
+        }
+
+        System.out.println("=================================================================");
+        // thenApply()接收supplyAsync()的执行结果，返回的是CompletableFuture<String>
+        try {
+            CompletableFuture<String> future = CompletableFuture
+                    .supplyAsync(() -> "hello", threadPoolExecutor)
+                    .thenApply(s -> s + ", thenApply");
+            System.out.println("thenApply result: " +future.get());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("=================================================================");
+        // thenAccept()接收supplyAsync()的执行结果，返回的是CompletableFuture<Void>
+        try {
+            CompletableFuture<Void> future = CompletableFuture
+                    .supplyAsync(() -> "hello", threadPoolExecutor)
+                    .thenAccept(s -> System.out.println(s + ", thenAccept"));
+            System.out.println("thenAccept result: " + future.get());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("=================================================================");
+        // thenRun()不接收任务的结果，继续执行任务逻辑，返回的是CompletableFuture<Void>
+        try {
+            CompletableFuture<Void> future = CompletableFuture
+                    .supplyAsync(() -> "hello , thenRun", threadPoolExecutor)
+                    .thenRun(() -> {
+                        System.out.println("thenRun ..............");
+                    });
+            System.out.println("thenRun result: " + future.get());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("=================================================================");
+        // thenCompose()，编排两个存在依赖关系的任务，和thenApply()的核心不同之处在于它们的返回值类型
+        // thenApply()：返回计算结果的原始类型，比如返回String;
+        // thenCompose()：返回CompletableFuture类型，比如返回CompletableFuture.
+        try {
+            CompletableFuture<String> future = CompletableFuture
+                    .supplyAsync(() -> "hello", threadPoolExecutor)
+                    .thenCompose(s -> CompletableFuture.supplyAsync(() -> s + ", thenCompose"));
+            System.out.println("thenCompose result: " + future.get());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("=================================================================");
+        // thenCombine()组合两个相互独立的任务
+        try {
+            CompletableFuture<Integer> positiveNumber = CompletableFuture.supplyAsync(() -> 500, threadPoolExecutor);
+            CompletableFuture<Integer> negativeNumber = CompletableFuture.supplyAsync(() -> -400, threadPoolExecutor);
+
+            CompletableFuture<Object> future = positiveNumber
+                    .thenCombine(negativeNumber, (positive, negative) -> {
+                        positive++;
+                        negative--;
+                        return positive + negative;
+                    });
+            System.out.println("thenCombine result: " + future.get());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
         }
 
         // 手动关闭线程池
