f9a44aaaf8250570b161b03f67e857a040a1b43a
Hoshino5206 <1077803190@qq.com>
2023-05-25 18:43:06 +0800

    (springboot-interceptor):fix @requestMapping

--- springboot-interceptor/src/main/java/com/hoshino/springboot/interceptor/controller/UserController.java
+++ springboot-interceptor/src/main/java/com/hoshino/springboot/interceptor/controller/UserController.java
@@ -3,6 +3,7 @@ package com.hoshino.springboot.interceptor.controller;
 import com.hoshino.springboot.interceptor.common.ResponseResult;
 import com.hoshino.springboot.interceptor.entity.User;
 import com.hoshino.springboot.interceptor.service.UserServiceImpl;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class UserController {
     @Resource
     private UserServiceImpl userServiceImpl;
 
-    @RequestMapping("/list")
+    @PostMapping("/list")
     public ResponseResult<List<User>> userList() {
         List<User> result = userServiceImpl.getUserList();
         return ResponseResult.success(result);
