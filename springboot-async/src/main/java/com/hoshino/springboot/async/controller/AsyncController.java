6c15031c8f0da85fd2cab26e935a974102d7c7bd
Hoshino5206 <1077803190@qq.com>
2023-05-26 17:33:18 +0800

    (springboot-async): update code

--- springboot-async/src/main/java/com/hoshino/springboot/async/controller/AsyncController.java
+++ springboot-async/src/main/java/com/hoshino/springboot/async/controller/AsyncController.java
@@ -1,36 +1,28 @@
 package com.hoshino.springboot.async.controller;
 
-import com.hoshino.springboot.async.task.AsyncTask;
+import com.hoshino.springboot.async.service.AsyncService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 /**
  * @author huangyuehao
  * @date 2023-05-16
  */
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("/async/admin")
 @Slf4j
 public class AsyncController {
 
     @Resource
-    private AsyncTask asyncTask;
+    private AsyncService asyncService;
 
-    @PostMapping("/asyncTask")
-    public void asyncTask() throws InterruptedException, ExecutionException {
-
-        asyncTask.dealNoReturnTask();
-
-        Future<String> result = asyncTask.dealHaveReturnTask(5);
-
-        log.info("主线程执行finished");
-        log.info(result.get());
+    @RequestMapping("/task")
+    public void asyncTask() throws ExecutionException, InterruptedException {
+        asyncService.dealAsyncTask();
     }
 
 }
